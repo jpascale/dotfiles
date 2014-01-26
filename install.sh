2a6dc46874bd1bc274d283aeac4699b04d588bc6
Cristian Pereyra <cristian@redmintlabs.com>
2014-01-26 15:50:34 -0300

    add git extras by visionmedia

--- install.sh
+++ install.sh
@@ -8,9 +8,19 @@ echo "Installing oh my zsh!"
    echo "Remove your .zshrc if you want it to be replaced!") || \
    ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
+echo "OK!"
+
 echo "Setting up git"
 
+echo "--- Linking"
 ([ -f $HOME/.gitconfig ] && \
    echo "Remove your .gitconfig if you want it to be replaced!") || \
    (ln -s $HOME/.dotfiles/.gitconfig $HOME/.gitconfig && ln -s $HOME/.dotfiles/.githelpers $HOME/.githelpers)
 
+echo "--- OK!"
+
+echo "--- Git extras"
+
+(cd /tmp && git clone --depth 1 https://github.com/visionmedia/git-extras.git && cd git-extras && sudo make install)
+
+echo "--- OK!"
