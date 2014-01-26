76da441cf4243ae1d575389139aa044bc7b9a527
Cristian Pereyra <cris@meg.local>
2014-01-26 15:37:23 -0300

    add script to install zshrc

--- install.sh
+++ install.sh
@@ -1,1 +1,9 @@
 #!/bin/zsh
+
+set -e
+
+echo "Installing oh my zsh!"
+
+([ -f $HOME/.zshrc ] && \
+   echo "Remove your .zshrc if you want it to be replaced!") || \
+   ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
