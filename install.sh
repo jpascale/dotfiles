3fcd1ae61769fa123123144112378cfc0251b0d9
Cristian Pereyra <cristian@redmintlabs.com>
2014-01-26 15:46:12 -0300

    add better settings for git

--- install.sh
+++ install.sh
@@ -7,3 +7,10 @@ echo "Installing oh my zsh!"
 ([ -f $HOME/.zshrc ] && \
    echo "Remove your .zshrc if you want it to be replaced!") || \
    ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+
+echo "Setting up git"
+
+([ -f $HOME/.gitconfig ] && \
+   echo "Remove your .gitconfig if you want it to be replaced!") || \
+   (ln -s $HOME/.dotfiles/.gitconfig $HOME/.gitconfig && ln -s $HOME/.dotfiles/.githelpers $HOME/.githelpers)
+
